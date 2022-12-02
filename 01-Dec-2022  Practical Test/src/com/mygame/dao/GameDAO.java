0b04a7f6c37243a46b973e34f9c911176a5c7465
Hp <Hp@LAPTOP-9SJP2RGU>
2022-12-02 19:43:48 +0530

    updated version 6

---
+++ 01-Dec-2022  Practical Test/src/com/mygame/dao/GameDAO.java
@@ -0,0 +1,11 @@
+package com.mygame.dao;
+
+public class GameDAO {
+static String CityName[]= {"Chennai","Mumbai","Delhi","Kolkata","Hyderabad"};
+public String[]getCityName(){
+	return CityName;
+	
+
+	}
+
+}
